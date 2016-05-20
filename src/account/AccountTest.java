b72a25d4f4ce079e7d46cf35d2ce31d2aafcf629
MD FARHAN CHOWDHURY <farhanchowdhury46@yahoo.com>
2016-05-19 22:10:13 -0400

    Update AccountTest.java

--- src/account/AccountTest.java
+++ src/account/AccountTest.java
@@ -14,7 +14,7 @@ public class AccountTest
 {
     public static void main( String[] args )
     {
-        Account account1 = new Account( 50.00 ); //Pre-data to use
+        Account account1 = new Account( 70.00 ); //Pre-data to use
         Account account2 = new Account( -7.53 ); //Pre-data to use
         
         System.out.printf( "account1 balance: $%.2f\n",
